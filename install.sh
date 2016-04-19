f992c9c97acc868b1d1fe27bd584eba97c4d03f9
MLambeth711 <m.d.lambeth@gmail.com>
2016-04-19 13:30:04 -0400

    Making the installer
    Designed to make setup of the system automated and easier on us

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+echo "This will get the base packages for the Streamlined Linux system. Run as Super User/sudo"
+ROOT_UID=0
+if [ $UID != $ROOT_UID ]; then
+    echo "You don't have sufficient privileges to run this script."
+    exit 1
+fi
+#This will install the K desktop environment with sufficient drivers.
+LIST_OF_APPS="xf86-video-fbdev plasma-meta chromium xorg-server xf86-video-intel	mesa-libgl	lib32-mesa-libgl ssdm lollypop"
+sudo pacman -S $LIST_OF_APPS
+sudo pacman -R lollypop
+sudo systemctl enable sddm.service
